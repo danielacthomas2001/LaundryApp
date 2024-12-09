4b16327f8a130e277b7f7b565285428ca2561fba
andrewxhc <xuhanchengandrew@gmail.com>
2024-12-08 21:34:40 -0500

    database changed

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(cors());
 
 const db = mysql.createPool({
@@ -11,8 +14,22 @@ const db = mysql.createPool({
     host            : 'localhost',
     user            : 'root',
     password        : '',
-    database        : 'LaundryApp'
+    database        : 'laundry-app-group'
 });
+
+
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    db.query("INSERT INTO user (email, password) VALUES (?, ?)", [email, password], (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send({email: email})
+        }
+    })
+})
+
 
 app.get('/', (req, res) => {
     db.query("INSERT INTO usertest (username, password) VALUES ('Testing', '123')", (err, result) => {
